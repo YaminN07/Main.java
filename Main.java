0ce56a9c9912f24de551d02cb87543a9ac975bce
YaminN07 <144473898+YaminN07@users.noreply.github.com>
2024-01-03 22:12:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
-import javax.swing.JFrame;
 
-public class Maze extends JFrame{
-  public Maze(){
-    super("Maze");
-    setSize(900,600);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setResizable(false);
-    add(new Contents());
-    setLocationRelativeTo(null);
-    setVisible(true);
+public class Main {
+  public static void main(String[] args) {
+    Maze maze= new Maze();
+    
   }
 }
  
