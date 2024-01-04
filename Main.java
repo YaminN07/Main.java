9bcf358f781164adc09b9f7a6e49003308e8d15e
YaminN07 <144473898+YaminN07@users.noreply.github.com>
2024-01-03 22:10:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,50 +1,14 @@
-public class Algorithm //4.1
-{
-  public int numDigits(int x)
-  {
-    String str=Integer.toString(x);
-    x= str.length();
-    return x;
-  }
-  public boolean isDivisible(int x ,int y)
-  {
-    boolean b=((x%y)==0);
-    return b;
-  }
-  public int sumOfOddRange(int x,int y)
-  {
-    int sum=0;
-    for (i=x, i<=y, i++)
-    {
-      if (i%2 !=0)
-      {
-        sum+=i;
-      }
-      sum+=0; 
-    }
-    return sum;
-   }
-  public int sumOfMultiple10Rage(int x, inty)
-  {
-    int sum=0;
-    for (i=x, i<=y, i++)
-    {
-      if (i%10 ==0)
-      {
-        sum+=i;
-      }
-      sum+=0; 
-    }
-    return sum;
-   }
-  public int sumOfDigits(int x)
-  {
-    int sum=0;
-    while(x>0)
-    {
-      sum+=x%10;
-      x=x/10;  
-    }
-    return sum;
+import javax.swing.JFrame;
+
+public class Maze extends JFrame{
+  public Maze(){
+    super("Maze");
+    setSize(900,600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+    add(new Contents());
+    setLocationRelativeTo(null);
+    setVisible(true);
   }
 }
+ 
