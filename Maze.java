8c419c7e4363526711d6d73932c2916e398b920c
YaminN07 <144473898+YaminN07@users.noreply.github.com>
2024-01-03 22:13:14 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,1 +1,14 @@
+import javax.swing.JFrame;
 
+public class Maze extends JFrame{
+  public Maze(){
+    super("Maze");
+    setSize(900,600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+    add(new Contents());
+    setLocationRelativeTo(null);
+    setVisible(true);
+  }
+}
+ 
