9f650cdc73f41b1359ea8ad95e0a6a59754ea5c9
YaminN07 <144473898+YaminN07@users.noreply.github.com>
2024-01-04 12:33:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-
+import javax.swing.JFrame;
 public class Main {
   public static void main(String[] args) {
-    Maze maze= new Maze();
-    
+    //Maze maze= new Maze();
+    JFrame f = new JFrame();
+    Contents c = new Contents();
+    f.add(c);
+    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setSize(800, 600);
   }
 }
  
